153086875212f8040cc6f2b1f343f1821f0d61c1
Gabriel Alexandre <gabriel.castilho@outlook.com.br>
2021-05-10 22:42:36 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <h4 class="is-size-4">Pokedex</h4>
+      <hr>
+      <input class="input is-rounded" type="text" name="" id="" placeholder="Procurar Pokemón" v-model="find">
+      <button class="button is-fullwidth is-info" id="find-btn" @click="search">Buscar</button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
+export default {
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+      filteredPokemons:[],
+      find:''
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0.")
+      .then((res) => {
+        console.log("pegou a lista");
+        this.pokemons = res.data.results;
+        this.filteredPokemons=res.data.results;
+      });
+  },
+  components: {
+    Pokemon,
+  },
+  methods:{
+    search : function(){
+       this.filteredPokemons = this.pokemons;
+       if(this.find =='' || this.busca==' '){
+         this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons= this.pokemons.filter(pokemon=>pokemon.name ==this.find)
+      }
+    }
+  },
+  computed:{
+    /*
+    searchResult:function(){
+      if(this.find =='' || this.busca==' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon =>pokemon.name == this.find)
+      }
+    }*/
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#find-btn{
+  margin-top: 2%;
+}
+</style>
